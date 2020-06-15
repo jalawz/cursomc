6e89a878e009018322a5c30e5379f2e32f78affa
ice_pmenezes <ice_pmenezes@uolinc.com>
2020-06-15 16:47:36 -0300

    Banco de dados h2 e criação da base

--- src/main/java/com/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/domain/Categoria.java
@@ -3,15 +3,23 @@ package com.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -1447282662484019830L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;

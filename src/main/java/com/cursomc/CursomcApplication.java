49db95760495378c79d3bc1788797b1974b6b3ab
ice_pmenezes <ice_pmenezes@uolinc.com>
2020-06-15 17:02:53 -0300

    18 - Criando operacao de instanciacao

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.cursomc;
 
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
+
+import com.cursomc.domain.Categoria;
+import com.cursomc.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @PostConstruct
+    void init () {
+        final Categoria cat1 = new Categoria(null, "Informática");
+        final Categoria cat2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
+
+    public static void main (String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
 
 }

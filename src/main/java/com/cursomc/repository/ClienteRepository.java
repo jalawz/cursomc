1c2a2908567ce37fe9107c800bd78a0ef9fb1a12
ice_pmenezes <ice_pmenezes@uolinc.com>
2020-07-27 16:31:33 -0300

    46 - Validacao customizada: email nao repetido na insercao de cliente

--- src/main/java/com/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/cursomc/repository/ClienteRepository.java
@@ -3,8 +3,11 @@ package com.cursomc.repository;
 import com.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    @Transactional(readOnly = true)
+    Boolean existsByEmail (String email);
 }
--- src/main/java/com/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/service/validation/ClienteInsertValidator.java
@@ -8,10 +8,15 @@ import javax.validation.ConstraintValidatorContext;
 
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repository.ClienteRepository;
 import com.cursomc.resources.exception.FieldMessage;
 import com.cursomc.service.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize (final ClienteInsert constraintAnnotation) {
@@ -28,6 +33,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (value.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCPF(value.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
+        }
+
+        if (clienteRepository.existsByEmail(value.getEmail())) {
+            list.add(new FieldMessage("email", "Email ja existente"));
         }
 
         list.forEach(field -> {
